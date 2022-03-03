9623d5bdbcb96e5af047d2fd4f0bac8fb4f98eea
hjwhuge <hujiawei_h@163.com>
2022-03-03 22:34:35 +0800

    feat: 修改 deploy.sh 发布文件

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run docs:build
+yarn docs:build
 
 # navigate into the build output directory
 cd docs/.vitepress/dist
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<hjwhuge>/<hjwhuge>.github.io.git master
+git push -f git@github.com:hjwhuge/hjwhuge.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
